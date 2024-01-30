7c339263cbcdde9f3d73559007e6b361c51d5ea0
bimapriaaditya <bpabima@gmail.com>
2024-01-30 07:09:15 +0700

    Component

---
+++ resources/js/Components/Lists/CardList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardList = () => {
+  return (
+    <div>
+      <span>Project List</span>
+    </div>
+  );
+}
+
+export default CardList;
--- resources/js/Pages/Projects/List.jsx
+++ resources/js/Pages/Projects/List.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import CardList from '@/Components/Lists/CardList';
 
 const breadcrumb = [
   { name: "Project", href: route('dashboard') },
   { name: "List" }
 ]
 
-const List = ({auth, data}) => {
+const List = ({ auth, data }) => {
+
+  console.log(data)
 
   return (
     <>
-    <div className="py-12">
-      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-          <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-            Lorem Ipsum dolor
-          </div>
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+          {
+            data.map()
+          }
+        </div>
       </div>
-    </div>
     </>
   );
 }
@@ -26,8 +29,8 @@ List.layout = page => (
   <AuthenticatedLayout
     user={page.props.auth.user}
     header={breadcrumb}>
-      <Head title="Project List" />
-      { page }
+    <Head title="Project List" />
+    {page}
   </AuthenticatedLayout>
 )
 
